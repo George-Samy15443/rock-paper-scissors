043654bfb5bddaabfcc375ddb222871b9b828de8
George-Samy <91060888+George-Samy15443@users.noreply.github.com>
2026-07-27 18:13:11 +0300

    Added computer choice logic

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3) + 1
+    console.log(randomNumber);
+    
+    let guess = ''
+    if(randomNumber === 1)
+        guess = 'rock'
+    else if(randomNumber === 2)
+        guess = 'paper'
+    else
+        guess = 'scissors'
+    return guess
+}
+
+console.log(getComputerChoice());
+
